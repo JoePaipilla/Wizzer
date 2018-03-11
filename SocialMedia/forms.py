f9623c30bee297cd431b3ff52bf9f92a7571f0b8
Joe Paipilla <paipillajoe@yahoo.com>
2018-03-11 04:44:52 -0400

    ok

--- SocialMedia/forms.py
+++ SocialMedia/forms.py
@@ -1,14 +1,17 @@
 from django import forms
 from django.contrib.auth.models import User
 
+
 class WhizForm(forms.Form):
     whiz_input = forms.CharField(label="", widget=forms.Textarea(attrs={"cols": '',"rows": '',"id": "message","placeholder":"Write a post!","value": "message"}))
+
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ['username', 'password']
+
 
 class LoginForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
